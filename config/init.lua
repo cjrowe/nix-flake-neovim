b66c843ce6ad551c8e375f240e10e7c919870ff7
Chris Rowe <2837323+cjrowe@users.noreply.github.com>
2024-12-19 16:20:18 +0000

    Create init.lua

---
+++ config/init.lua
@@ -0,0 +1,9 @@
+-- Load core options
+require('options')
+
+-- Load plugin configurations
+require('plugins.ale')
+require('plugins.cmp')
+require('plugins.lualine')
+require('plugins.telescope')
+require('plugins.treesitter')
