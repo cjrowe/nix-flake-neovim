2ef647441edfd666a6809025b334cc788088db99
Chris Rowe <2837323+cjrowe@users.noreply.github.com>
2024-12-19 16:23:36 +0000

    Create telescope.lua

---
+++ config/lua/plugins/telescope.lua
@@ -0,0 +1,23 @@
+local telescope = require('telescope')
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = "move_selection_next",
+        ["<C-k>"] = "move_selection_previous",
+      },
+    },
+    file_ignore_patterns = {
+      "node_modules",
+      ".git",
+      "%.lock"
+    }
+  },
+})
+
+-- Key mappings for telescope
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true})
